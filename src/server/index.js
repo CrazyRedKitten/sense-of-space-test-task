941b2b029c96929c0a11577d19b67ef6ea09cbed
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2021-07-29 16:11:01 +0300

    Project setup 2

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+// TODO: Add post request handling
+app.post('/api/image', (req, res) => {
+  // TODO: Add image processing
+  res.send('POST request to the homepage');
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  // TODO: Change clinetDir value to dinamic instead of hardcoded
+  const clientDir = '/Users/crazyredkitten/Documents/'+
+  'GitHub/sense-of-space-test-task/src/client';
+  res.sendFile(path.join(clientDir, 'index.html'));
 });
 
 app.listen(port, () => {
